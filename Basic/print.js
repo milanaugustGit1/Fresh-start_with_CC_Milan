c3e42554b182d184becebf6cfe6fe9f759740862
milanaugustGit1 <milanaugust1@gmail.com>
2025-12-21 07:52:39 +0000

    print 1 to 10 number basic integer code for beginner

---
+++ Basic/print.js
@@ -0,0 +1,19 @@
+/*  1️⃣ Print numbers from 1 to 10
+
+Use a loop to print:
+
+```
+1
+2
+3
+...
+10
+
+```
+
+Goal: Understand basic `for` loop structure.
+*/
+
+for(let i=1; i<=10; i++){
+    console.log(i);
+}
